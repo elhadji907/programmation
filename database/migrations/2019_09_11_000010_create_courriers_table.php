0fc288d5d893ad7aa33734d2b3423313784b485e
dev <badjilaminefbkk@gmail.com>
2019-10-05 13:09:43 +0000

    update

---
+++ database/migrations/2019_09_11_000010_create_courriers_table.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourriersTable extends Migration
+{
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $tableName = 'courriers';
+
+    /**
+     * Run the migrations.
+     * @table courriers
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->char('uuid', 36);
+            $table->string('numero', 200);
+            $table->string('name', 200)->nullable();
+            $table->string('types', 200)->nullable();
+            $table->string('message', 200)->nullable();
+            $table->string('destinataire', 200)->nullable();
+            $table->string('fichier', 200)->nullable();
+            $table->string('statut', 200)->nullable();
+            $table->timestamp('date')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->unsignedInteger('gestionnaires_id');
+            $table->unsignedInteger('users_id');
+            $table->unsignedInteger('types_courriers_id');
+
+            $table->index(["gestionnaires_id"], 'fk_courriers_gestionnaires1_idx');
+
+            $table->index(["users_id"], 'fk_courriers_users1_idx');
+
+            $table->index(["types_courriers_id"], 'fk_courriers_types_courriers1_idx');
+            $table->softDeletes();
+            $table->nullableTimestamps();
+
+
+            $table->foreign('gestionnaires_id', 'fk_courriers_gestionnaires1_idx')
+                ->references('id')->on('gestionnaires')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('users_id', 'fk_courriers_users1_idx')
+                ->references('id')->on('users')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('types_courriers_id', 'fk_courriers_types_courriers1_idx')
+                ->references('id')->on('types_courriers')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists($this->tableName);
+     }
+}
---
+++ modelgenerator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+php artisan make:model Role
+php artisan make:model Arrive
+php artisan make:model Consommation
+php artisan make:model Reglement
+php artisan make:model Type
+php artisan make:model Gestionnaire
+php artisan make:model User
