b41d7accfdded1feefe7e396df39eb482ec67b77
dev <badjilaminefbkk@gmail.com>
2019-10-12 18:31:06 +0000

    update

--- resources/views/courriers/index.blade.php
+++ resources/views/courriers/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.default')
 @section('content')
 <div class="container-fluid">
-    <div class="col-md-12">
+    {{-- <div class="col-md-12">
         <a class="btn btn-info btn-block" href="{{ route('arrives.show') }}"><span data-feather="book-open"></span> Courriers arrivés</a>
     </div>
     <br />
@@ -11,6 +11,87 @@
     <br />
     <div class="col-md-12">
         <a class="btn btn-info btn-block" href="#"><span data-feather="book-open"></span> Courriers internes</a>
+    </div> --}}
+
+    <div class="row">
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">{{ ('Courriers (ANNUELS)') }}</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Courrier::get()->count() }}</div>
+                  </div>
+                  <div class="col-auto">
+                    {{-- <i class="fas fa-calendar fa-2x text-gray-300"></i> --}}
+                    <span data-feather="mail"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">{{ ('Courriers (ARRIVES)') }}</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Recue::get()->count() }}</div>
+                  </div>
+                  <div class="col-auto">
+                    {{-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> --}}
+                    <span data-feather="mail"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+          <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">{{ ('Courriers (DEPARTS)') }}</div>
+                        <div class="row no-gutters align-items-center">
+                          <div class="col-auto">
+                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ \App\Depart::get()->count() }}</div>
+                          </div>
+                         {{--  <div class="col">
+                            <div class="progress progress-sm mr-2">
+                              <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                          </div> --}}
+                        </div>
+                      </div>
+                      <div class="col-auto">
+                        {{-- <i class="fas fa-clipboard-list fa-2x text-gray-300"></i> --}}
+                        <span data-feather="mail"></span>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                  <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                      <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ ('Courriers (INTERNES)') }}</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Interne::get()->count() }}</div>
+                      </div>
+                      <div class="col-auto">
+                        {{-- <i class="fas fa-comments fa-2x text-gray-300"></i> --}}
+                        <span data-feather="mail"></span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
     </div>
 </div>
 <hr />
