0b0e5ef0c7289c03682ad70bf6bbe642a182b244
dev <badjilaminefbkk@gmail.com>
2019-10-28 18:21:44 +0000

    update

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,8 +4,8 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-      <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+      <h1 class="h3 mb-0 text-gray-800">Tableau de bord</h1>
+      {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
     </div>
 
     <!-- Content Row -->
@@ -14,17 +14,20 @@
       <!-- Earnings (Monthly) Card Example -->
       <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-primary shadow h-100 py-2">
+            <a class="nav-link" href="{{ route('courriers.index') }}">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
-                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Earnings (Monthly)</div>
-                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">COURRIERS (TOTAL)</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Courrier::get()->count() }}</div>
               </div>
               <div class="col-auto">
-                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                {{-- <i class="fas fa-calendar fa-2x text-gray-300"></i> --}}
+                <span data-feather="mail"></span>
               </div>
             </div>
-          </div>
+          </div> 
+          </a>
         </div>
       </div>
 
