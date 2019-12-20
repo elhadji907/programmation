380634be895e76a18de12a2ae7076f4a857a43b6
dev <badjilaminefbkk@gmail.com>
2019-12-20 10:23:55 +0000

    update

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -15,8 +15,7 @@
   <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
 
 
- {{--   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>  --}}
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+  <script src="{{ asset('dist/js/select2.min.js') }}"></script>
   
   <script type="text/javascript">
   
