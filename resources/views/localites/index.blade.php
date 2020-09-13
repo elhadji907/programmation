99ac353fcb4db43de9ca2740185c9fd865360a5c
dev <badjilaminefbkk@gmail.com>
2020-09-13 12:42:04 +0000

    update

--- resources/views/localites/index.blade.php
+++ resources/views/localites/index.blade.php
@@ -24,7 +24,7 @@
                 <a href="{!! url('localites/create') !!}"><div class="btn btn-success  btn-sm"><i class="fas fa-plus"></i>&nbsp;Ajouter</div></a> 
               </div>
                 <br />
-              <table class="table table-bordered table-striped" id="dataTable" width="100%" cellspacing="0">
+              <table class="table table-bordered table-striped" id="localiteTable" width="100%" cellspacing="0">
                 <thead class="table-dark">
                   <tr>
                     <th>N°</th>
@@ -78,3 +78,50 @@
     </div>
   </div>
 @endsection
+@push('scripts')
+<script type="text/javascript">
+  $(document).ready( function () {
+    $('#localiteTable').DataTable({
+      dom: 'lBfrtip',
+      buttons: [
+          'copy', 'csv', 'excel', 'pdf', 'print',
+      ],
+      "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "Tout"] ],
+      "order": [
+            [ 0, 'asc' ]
+            ],
+            language: {
+              "sProcessing":     "Traitement en cours...",
+              "sSearch":         "Rechercher&nbsp;:",
+              "sLengthMenu":     "Afficher _MENU_ &eacute;l&eacute;ments",
+              "sInfo":           "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+              "sInfoEmpty":      "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
+              "sInfoFiltered":   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+              "sInfoPostFix":    "",
+              "sLoadingRecords": "Chargement en cours...",
+              "sZeroRecords":    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+              "sEmptyTable":     "Aucune donn&eacute;e disponible dans le tableau",
+              "oPaginate": {
+                  "sFirst":      "Premier",
+                  "sPrevious":   "Pr&eacute;c&eacute;dent",
+                  "sNext":       "Suivant",
+                  "sLast":       "Dernier"
+              },
+              "oAria": {
+                  "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
+                  "sSortDescending": ": activer pour trier la colonne par ordre d&eacute;croissant"
+              },
+              "select": {
+                      "rows": {
+                          _: "%d lignes séléctionnées",
+                          0: "Aucune ligne séléctionnée",
+                          1: "1 ligne séléctionnée"
+                      }
+              }
+            }
+    });
+} );
+  
+</script>
+@endpush
+
